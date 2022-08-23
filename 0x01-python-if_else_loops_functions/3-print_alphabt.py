ea323bfe31e6d2f068388a54823272f6ae641d67
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-08-23 12:05:13 -0700

    ASCII alphabet, in lowercase except q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range (97, 123):
-    if i == 101 or i == 113:
-        continue
-    print("{}".format(chr(i)), end='')
+for i in range (26):
+    if i != 4 and i != 16:
+        print("{:s}".format(chr(i + ord("a"))), end="")
