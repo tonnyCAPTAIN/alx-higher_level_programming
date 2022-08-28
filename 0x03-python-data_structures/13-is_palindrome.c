da3cb5659e8c13485735b2d45ac7d5bab8101853
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-08-28 05:59:51 -0700

    Linked list palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ * Return: Always 0 on success
+ */
+
+int main(void)
+{
+	listint_t *head;
+	return (0);
+}
