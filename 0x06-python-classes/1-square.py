52b7c8d61710f33d512c174c9164d99220f477df
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-09-12 11:16:03 -0700

    Private instance attribute: size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Defining a class square """
+
+class Square:
+    """Represent a square"""
+    def __init__(self, size):
+        """INitilise a new Square.
+
+        Args:
+         size (int): The size of the new square.
+         """
+         self.__size = size
