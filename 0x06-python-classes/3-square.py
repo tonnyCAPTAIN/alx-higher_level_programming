9f244f935a25b2d835c732966e1108de79e351c9
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-09-14 05:41:36 -0700

    are

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -19,8 +19,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-
-        
+  
     def area(self):
         """Find are of sqaure"""
         return(self.__size * self.__size)
