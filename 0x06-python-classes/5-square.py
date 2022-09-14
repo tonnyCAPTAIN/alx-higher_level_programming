4f7192a5b257ddafd98e5ea3a9abda67a8abf2c3
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-09-14 06:27:11 -0700

    print # square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -32,7 +32,7 @@ class Square:
         def my_print(self):
             """ printing the sqaure"""
             for i in range(0, self.__size):
-                [print("#", end="") for j in range(self.__size)]
-                print("")
+                for j in range(0, self.__size):
+                    print("#", end="")
             if self.__size == 0:
                 print("")
