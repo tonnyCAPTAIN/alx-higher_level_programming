ec7d15a2ca08edb3fc3e1b9bd728929e1add9cf8
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-09-16 08:41:12 -0700

    Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """ returns addition of a and b
+    Args:
+        a = 1st num
+        b = 2nd num
+
+    TypeError: If a or b aren't integer and/or float numbers
+    """
+    if type(a) == float or type(b) == float:
+        a = int(a)
+        b = int(b)
+
+   if type(a) != int:
+       raise TypeError("a must be an integer")
+   elif type(b) != int:
+       raise TypeError("b must be an integer")
+   else:
+       return a + b
+
+
