0d71f7d24da38c6fa6441f5dd4d79c8863051b58
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-09-20 12:54:55 -0700

    magic string

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string.counter = getattr(magic_string, "counter", 0) + 1
-    return ", ".join(["Holberton" for i in range(magic_string.counter)])
+def magic_string(H=[]):
+    H += ["BestSchool"]
+    return (", ".join(H))
