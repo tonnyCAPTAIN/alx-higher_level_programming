7dbebcfd500a42203a24d2e2d90a184436524523
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-09-23 04:30:32 -0700

    Revert "print square"
    This reverts commit cc69fb34cdbade8a06c46edd9e0e2205f711b902.
    
    revertt

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -4,21 +4,13 @@
 Args:
     size- should be integer above 0 only
 """
-
-
 def print_square(size):
-    """This function prints a square with the character #
-    Args:
-        size (int): This represents the length of the square
-    """
-    
+    """print square with #"""
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
-    elif size < 0:
+    if size < 0:
         raise ValueError("size must be >= 0")
-    elif isinstance(size, float) and size < 0:
-        raise TypeError("size must be an integer")
-    for n in range(0, size):
-        for m in range(size):
+    for i in range(size):
+        for j in range(size):
             print("#", end="")
-        print("")
+        print()
