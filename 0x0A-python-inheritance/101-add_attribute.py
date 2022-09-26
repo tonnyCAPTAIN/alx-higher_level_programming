5cc403f829b58fdab6b933bf75f9b2d20a2a36e9
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-09-26 13:06:59 -0700

    function that adds a new attribute to an object if it’s possible:

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,7 +6,6 @@ def add_attribute(a_class, name, value):
     """Adds new attribute to an object if it's possible"""
 
     cannot_add = {int, str, float, list, dict, tuple, frozenset, type, object}
-
     if type(a_class) in cannot_add:
         raise TypeError("can't add new attribute")
 
