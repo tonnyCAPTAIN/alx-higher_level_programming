94fd29f2d6aa4bc9228543674405944c54c34ffd
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-09-27 06:18:00 -0700

    add a text and return th number of text added

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""appending a string at the end of a text file
+and returns the number of characters added
+"""
+
+
+def append_write(filename="", text=""):
+    """append text to filename"""
+    with open(filename, 'r+') as f:
+        count = 0
+        f = f.write(text)
+        for i in text:
+            count += 1
+
+        return count
