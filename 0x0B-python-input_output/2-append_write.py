0cb34d5e0896b490460ba97439a74da79e72bcab
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-09-27 06:36:45 -0700

    append to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,11 +7,5 @@ and returns the number of characters added
 def append_write(filename="", text=""):
     """append text to filename"""
 
-    with open(filename, 'W') as f:
-        count = 0
-        j = f.write(text)
-        last_line = j.readline()[-1]
-        for i in last_line:
-            count += 1
-
-        return count
+    with open(filename, 'a') as f:
+        return f.write(text)
