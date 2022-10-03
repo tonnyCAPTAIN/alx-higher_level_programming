469739bcdd0c6ca682c391eb3fe2a19b3bfbac06
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-10-03 10:25:38 -0700

    swaure

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -31,33 +31,22 @@ class Square(Rectangle):
 
     def __str__(self):
         """Returns string representation of Square """
-        return "[{}] ({}) {}/{} - {}".format(
-                self.__class__.__name__, self.id, self.x, self.y, self.width)
+        return"[Square] ({:d}) {:d}/{:d} - {:d}".format(
+            self.id, self.x, self.y, self.width)
 
     def update(self, *args, **kwargs):
-        """updating the square values"""
-
-        if len(args):
-            for i, arg in enumerate(args):
-                if i == 0:
-                    self.id = arg
-                elif i == 1:
-                    self.size = arg
-                elif i == 2:
-                    self.x = arg
-                elif i == 3:
-                    self.y = arg
-        else:
+        """Returns Arguments"""
+        update = ("id", "size", "x", "y")
+        length = len(args)
+        if args:
+            for i in range(length):
+                setattr(self, update[i], args[i])
+        elif not args or update < 1:
             for key, value in kwargs.items():
-                if hasattr(self, key) is True:
+                if key in update:
                     setattr(self, key, value)
 
     def to_dictionary(self):
-            """dictionary square represetation"""
-
-        return {
-                'id': self.id,
-                'x': self.x,
-                'size': self.width,
-                'y': self.y
-                 }
+        """ Returns the Squares's dict """
+        s_dict = {"id": self.id, "x": self.x, "size": self.size, "y": self.y}
+        return s_dict
