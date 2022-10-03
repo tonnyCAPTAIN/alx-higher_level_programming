ddfc833475297edec1800015e7fd9fc48541d2ec
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-10-02 18:35:01 -0700

    value correction

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -47,7 +47,7 @@ class Rectangle(Base):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
-        self.__height = height
+        self.__height = value
 
     @property
     def x(self):
