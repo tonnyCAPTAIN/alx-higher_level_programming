b98e718af6758a283c2d4e3fcadf3207dc56aed6
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-10-27 12:24:53 -0700

    fix 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -58,12 +58,14 @@ class Square:
         """
         prints the square with #
         """
+       for row in range(0, self.__size):
+            [print("#", end="") for col in range(self.__size)]
+            print("")
         if self.__size == 0:
             print("")
-            return
 
         [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
-            print("")
+            print("") 
