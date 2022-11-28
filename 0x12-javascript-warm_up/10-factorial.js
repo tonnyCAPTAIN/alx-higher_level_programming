39c6914aeadbcebd2145b50981b087e43796d172
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-11-28 03:16:45 -0800

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+
+function factorial (num) {
+  if (isNaN(num) || num <= 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+
+const num = parseInt(process.argv[2]);
+
+console.log(factorial(num));
