30af51d3f00faa0340db75593df686ab5fe38430
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-11-28 02:28:00 -0800

    print c is fun number of times

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,9 +2,9 @@
 
 const num = parseInt(process.argv[2]);
 if (num) {
- for (let i = 0; i < process.argv[2]; i++){
+  for (let i = 0; i < process.argv[2]; i++){
     console.log('C is fun'); 
  }
 } else {
-  console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences')i;
 }
