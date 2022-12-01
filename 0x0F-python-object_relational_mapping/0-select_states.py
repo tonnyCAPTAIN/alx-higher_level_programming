70b0ebbe25dd6f776633c80703bba40a20d4dc8e
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-12-01 12:14:16 -0800

    slect all states from a db in ASC

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Script that lists all states from a database"""
+
+from sys import agrv
+import MySQLdb
+
+if __name__ = "__main__":
+    con = MySQLdb.connect(
+            host="localhost", port=3306, user=argv[1],
+            password=argv[2], database=argv[3])
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    db = cursor.fetchall()
+    for i in db:
+        print(i)
+    cursor.close()
+    db.close()
