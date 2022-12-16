e9c1d3935d9be09c048bfd1699068ec0cec98757
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-12-16 05:30:12 -0800

    post

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-# Take in a URL and email, send POST request
-
-import sys
+"""It Sends POST"""
 import urllib.request
 import urllib.parse
+import sys
+
+
+def sender():
+    """This is the sender method"""
+    v = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(v)
+    data = data.encode("ascii")
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print(html.decode("utf-8"))
+
 
 if __name__ == "__main__":
-    values = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(values)
-    data = data.encode('utf-8')
-    req = urllib.request.Request(sys.argv[1], data)
-    with urllib.request.urlopen(req) as res:
-        print(res.read().decode('utf-8'))
+    sender()
