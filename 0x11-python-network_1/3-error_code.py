324f92c317372e3b709a46043b28ea153fb7c6d6
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-12-16 05:33:04 -0800

    error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
-# Take in a URL, send a request to URL, and dispaly body
+"""initialize"""
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+from sys import argv
 
-import sys
-import urllib.request
-import urllib.error
 
-if __name__ == "__main__":
-    req = urllib.request.Request(sys.argv[1])
+def errorCode():
+    """post values to a url and get error code"""
+    url = argv[1]
+
+    req = Request(url)
     try:
-        with urllib.request.urlopen(req) as res:
-            print(res.read().decode('utf-8'))
-    except urllib.error.URLError as e:
-        print("Error code: {}".format(e.code))
+        with urlopen(req) as response:
+            print(response.read().decode('utf-8'))
+    except HTTPError as e:
+        print('Error code: {}'.format(e.code))
+
+
+if __name__ == '__main__':
+    errorCode()
